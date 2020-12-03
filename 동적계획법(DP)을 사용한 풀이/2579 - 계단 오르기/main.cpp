dbe740c0b46815ba944aa4e372209b647c876221
eunyoungroh <roheunyoung@outlook.com>
2020-12-03 20:15:18 +0900

    계단 오르기 풀이

---
+++ 동적계획법(DP)을 사용한 풀이/2579 - 계단 오르기/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int S[301];
+long long D[301][2];
+
+int main(int argc, char** argv) {
+	
+	freopen("input.txt", "rt", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N;
+	cin >> N;
+	
+	N++; // 시작점을 앞에 추가함 
+	
+	S[1] = 0; // 시작점의 스코어는 0 
+	for(int i = 2; i <= N; i++){
+		cin >> S[i];
+	}	
+	
+	D[1][1] = S[1];
+	D[1][0] = S[1];
+	
+	for(int n = 2; n <= N; n++){
+		
+		if(n == 3){
+			D[n][1] = max(D[n-1][1], D[n-1][0]) + S[n];
+			D[n][0] = max(D[n-2][1], D[n-2][0]) + S[n];	
+		}
+		else {
+			
+			D[n][1] = D[n-1][0] + S[n];
+			D[n][0] = max(D[n-2][1], D[n-2][0]) + S[n];
+		
+		}
+		
+		cout << "D[" << n << "][1] = " << D[n][1] << "\n";
+		cout << "D[" << n << "][0] = " << D[n][0] << "\n";
+		
+	}
+	
+	cout << max(D[N][1], D[N][0]);
+	
+	return 0;
+}
