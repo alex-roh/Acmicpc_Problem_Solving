72450cb601c855d44265399d40d2c8d560cddb80
eunyoungroh <roheunyoung@outlook.com>
2020-12-08 17:57:18 +0900

    평범한 배낭 풀이 2

--- 동적계획법(DP)을 활용한 풀이/12865 - 평범한 배낭/main.cpp
+++ 동적계획법(DP)을 활용한 풀이/12865 - 평범한 배낭/main.cpp
@@ -5,35 +5,44 @@
 
 using namespace std;
 
+// D[N] : N만큼의 무게가 가방에 남았을 때 담을 수 있는 최댓값
 int D[10000000];
+bool selected[101]; 
 vector<pair<int, int>> items(101); // first - weight, second - value
-bool selected[101];
 int N, K;
 
-void DFS(int index, int weight, int value){
+void DFS(int remainingWeight, int value){
 	
-	if(index > N){
+	if(remainingWeight <= 0){
 		return;
 	}
 	else {
-		for(int i = index; i <= N; i++){
-			
-			int weightAfter = weight + items[i].first;
-			int valueAfter = value + items[i].second;
-			
-			
-			if(weightAfter > K){
-				continue;
-			}
-			if(D[weightAfter] > valueAfter){
-				continue;
-			}
-			
-			D[weightAfter] = valueAfter;
-			DFS(i + 1, weightAfter, valueAfter);
-		}		
-	}
 	
+		for(int i = 1; i <= N; i++){
+
+			int itemWeight = items[i].first;
+			int itemValue = items[i].second;
+
+			// 가방에 아이템을 담을 수 있고 해당 아이템을 이전에 담지 않은 경우에만			
+			if(selected[i] == false && remainingWeight >= itemWeight){
+				
+				int remainingWeightAfter = remainingWeight - itemWeight; // 해당 아이템을 담고 난 이후 가방의 남은 무게 
+				int valueAfter = D[remainingWeight] + itemValue;
+				
+				//최대값을 갱신하지 않은 경우 
+				if(D[remainingWeightAfter] < valueAfter){
+					
+					D[remainingWeightAfter] = valueAfter;
+					selected[i] = true;
+					DFS(remainingWeight - itemWeight, value);
+					selected[i] = false;
+					
+				}	 
+				
+			}
+		}	
+		
+	}
 }
 
 int main(int argc, char** argv) {
@@ -50,11 +59,10 @@ int main(int argc, char** argv) {
 		items[i] = make_pair(W, V);
 	}
 	
-	DFS(1, 0, 0);	
-	
+	DFS(K, 0);	
 	
 	int max = -1;
-	for(int i = 1; i <= K; i++){
+	for(int i = 0; i <= K; i++){
 		if(D[i] > max)
 			max = D[i];
 	}
