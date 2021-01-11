09fbba49222a59215b3b32556e13ebdd8c627079
eunyoungroh <roheunyoung@outlook.com>
2021-01-11 11:52:11 +0900

    맞춰봐 풀이

--- 브루트 포스(Brute Force)를 활용한 풀이/1248 - 맞춰봐/main.cpp
+++ 브루트 포스(Brute Force)를 활용한 풀이/1248 - 맞춰봐/main.cpp
@@ -3,25 +3,45 @@
 using namespace std;
 typedef long long ll;
 
-int dx[4] = { 0,0,1,-1 };
-int dy[4] = { -1,1,0,0 };
-const int MAX = 2000000000;
-
 using namespace std;
 
 int N;
 char S[11][11];
-vector<vector<int>> ans;
+vector<int> sequence(30);
 bool isPrinted;
 
-void findSequenceRecursive(int index, vector<int>& sequence, vector<int>& partialSum){
+bool check(int index){
 	
-	if(index > N && isPrinted == false){
+	// 조건을 충족하는지 확인 
+	int partialSum = 0;
+	for(int i = index; i >= 1; i--){
+		partialSum += sequence[i];
+		
+		if(S[i][index] == '0'){
+			if(partialSum != 0) return false;
+		}
+		else if(S[i][index] == '-'){
+			if(partialSum >= 0) return false;
+		}
+		else if(S[i][index] == '+'){
+			if(partialSum <= 0) return false;
+		}
+	}
+	
+	return true;
+	
+}
+
+int findSequenceRecursive(int index){
+	
+	cout << "findSequenceRecursive(" << index << ")\n";
+	
+	if(index > N){
 		for(int i = 1; i <= N; i++){
 			cout << sequence[i] << " ";
 		}
 		cout << "\n";
-		isPrinted = true;
+		return 10;
 	}
 	else {
 		
@@ -30,69 +50,25 @@ void findSequenceRecursive(int index, vector<int>& sequence, vector<int>& partia
 		// sequence[index]가 0인 경우
 		if(hint == '0'){
 			sequence[index] = 0;
-			// 부분합을 캐싱 
-			if(index > 1)
-				partialSum[index] = sequence[index] + partialSum[index - 1];
-			else 
-				partialSum[index] = sequence[index];
-			
-			// 다른 경우에 대해 재귀호출
-			findSequenceRecursive(index + 1, sequence, partialSum);
-			return;
+			return findSequenceRecursive(index + 1);
 		}
 		
 		// sequence[index]에 1(-1)부터 10(-10)까지 넣어봄 
 		for(int i = 1; i <= 10; i++){
 			
 			// sequence[index]가 음수일 경우 
-			if(hint == '-'){
+			if(hint == '-')
 				sequence[index] = -i;
-			}
 			// sequence[index]가 양수일 경우 
-			else {
-				sequence[index] = i;
-			}
-			
-			// 부분합을 캐싱 
-			if(index > 1)
-				partialSum[index] = sequence[index] + partialSum[index - 1];
 			else 
-				partialSum[index] = sequence[index];
+				sequence[index] = i;
 			
-		
-			// 조건을 충족하는지 확인
-			bool okay = true;
-			for(int j = 1; j <= index; j++){
-				for(int k = j; k <= index; k++){	
-					if(j == k) continue; 
-					char shouldBe = S[j][k];
-				
-					int result;
-				
-					if(j == 1)
-						result = partialSum[k];
-					else
-						result = partialSum[k] - partialSum[j - 1];
-					
-					// 조건을 만족하는 경우
-					if((result == 0 && shouldBe == '0')
-						|| (result < 0 && shouldBe == '-')
-						|| (result > 0 && shouldBe == '+')){}
-					// 조건을 만족하지 않는 경우 
-					else {
-						okay = false;
-						break;
-					}
-				}
-				if(okay == false) break;
-			} 
-		
-			if(okay == true){
-				findSequenceRecursive(index + 1, sequence, partialSum);	
-			}
+			if(check(index) && (findSequenceRecursive(index + 1) == 10))
+				return 10;
 		}
+		
+		return -1;
 	}
-	
 }
 
 int main(int argc, char** argv) {
@@ -113,12 +89,8 @@ int main(int argc, char** argv) {
 		}
 	}
 	
-	vector<int> sequence(N, -11);
-	vector<int> partialSum(N, -11);
-	
-	findSequenceRecursive(1, sequence, partialSum);
+	findSequenceRecursive(1);
 	
 	return 0;
 
 }
-
