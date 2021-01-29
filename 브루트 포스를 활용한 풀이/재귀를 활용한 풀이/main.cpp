a98a9190914fd5088a084f20b19796f6d467cbf1
eunyoungroh <roheunyoung@outlook.com>
2021-01-29 12:09:28 +0900

    2048 (easy) 풀이

---
+++ 브루트 포스를 활용한 풀이/재귀를 활용한 풀이/main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define bnd(a,b,c) (a >= 0 && b >= 0 && a < c && b < c)
+#define bnd2(a, b, c, d) (a >= 0 && b >= 0 && a < c && b < d)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define mp(a,b) make_pair(a, b)
+
+#define cig(a) cin.ignore(a, '\n')
+#define ci(a) cin >> a
+#define ci2(a,b) cin >> a >> b
+#define gtl(a) getline(cin, a)
+#define co(a) cout << a
+#define cos(a) cout << a << ' ' // cout with space
+#define col(a) cout << a << '\n' // cout with line 
+#define cl cout << '\n' // cout only line
+
+#define pb push_back
+#define fst first
+#define scd second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef deque<int> di;
+typedef priority_queue<int> pqi;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> ti3;
+typedef tuple<int, int, int, int> ti4;
+
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ll, ll, ll, ll> tl4;
+typedef stack<ll> sl;
+typedef queue<ll> ql;
+typedef priority_queue<ll> pql;
+
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+int ddx[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+int ddy[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
+
+const int MAX = 2000000000;
+
+int main(int argc, char** argv) {
+	
+	//freopen("input.txt", "rt", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	int arr[4] = { 2, 4, 8, 16 };
+	
+	rep(i, 0, 4){
+		cos(arr[i]);
+	}
+	cl;
+	
+	reverse(arr, arr + 4);
+	
+	rep(i, 0, 4){
+		cos(arr[i]);	
+	}
+	cl;
+
+	
+	return 0;
+}
+
