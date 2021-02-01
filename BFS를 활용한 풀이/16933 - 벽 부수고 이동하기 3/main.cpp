0c5309bee31bad2a014061ebcdedae8086f08fac
eunyoungroh <roheunyoung@outlook.com>
2021-02-01 14:47:01 +0900

    벽 부수고 이동하기 3번 풀이

--- BFS를 활용한 풀이/16933 - 벽 부수고 이동하기 3/main.cpp
+++ BFS를 활용한 풀이/16933 - 벽 부수고 이동하기 3/main.cpp
@@ -56,48 +56,52 @@ int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { 1, 0, -1, 0 };
 int ddx[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
 int ddy[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
-
 const int MAX = 2000000000;
-int N, M, K;
+
+int N, M, K; 
 int B[1001][1001];
-int dist[1001][1001][12];
+int dist[1001][1001][2][12]; // (x, y, day, k)
 int res = -1;
 
+// (x, y) - 현재 위치 / day - 1낮0밤 / used - 몇 개의 블록을 터뜨렸는지 
 typedef struct _Node{
 	
-	int x; int y; int used;
-	_Node(int px, int py, int pused): x(px), y(py), used(pused) {};
+	int x; int y; int day; int used;
+	_Node(int px, int py, int pday, int pused): x(px), y(py), day(pday), used(pused) {};
 	
 } Node;
 
+// 경계값 체크 
 bool bcheck(int nx, int ny){
 	return ((nx >= 1) && (ny >= 1) && (nx <= N) && (ny <= M));
 }
 
 int BFS(){
-	
+
 	queue<Node> que;
-	que.ps(Node(1, 1, 0));
+	que.ps(Node(1, 1, true, 0));
 	
-	rep(i, 0, 10){
-		dist[1][1][i] = 1;
+	rep(i, 0, K){
+		dist[1][1][1][i] = 1;
 	}
-	
+
 	while(!que.emt()){
 		
 		Node cur = que.frt(); que.pp();
 		
 		int x = cur.x;
 		int y = cur.y;
+		int day = cur.day; 
 		int used = cur.used;
 		
 		// 도착했으면 캐싱 
 		if(x == N && y == M){
-			if(res == -1) res = dist[x][y][used];
-			else res = min(res, dist[x][y][used]);
+			if(res == -1) res = dist[x][y][day][used];
+			else res = min(res, dist[x][y][day][used]);
 			continue;
 		}
 		
+		// 상하좌우로 이동 
 		rep(i, 0, 4){
 			
 			int nx = x + dx[i];
@@ -105,13 +109,13 @@ int BFS(){
 			
 			if(!bcheck(nx, ny)) continue;
 			
-			// 벽이고 폭파시킬 수 있는 경우 
-			if(B[nx][ny] == 1 && used < K){
+			// 벽이고 낮이어서 폭파시킬 수 있는 경우 
+			if(B[nx][ny] == 1 && day == 1 && used < K){
 				
 				// 이전에 폭파시킨 적이 없다면 폭파 
-				if(dist[nx][ny][used + 1] == 0){
-					que.ps(Node(nx, ny, used + 1));
-					dist[nx][ny][used + 1] = dist[x][y][used] + 1;				
+				if(dist[nx][ny][!day][used + 1] == 0){
+					que.ps(Node(nx, ny, !day, used + 1));
+					dist[nx][ny][!day][used + 1] = dist[x][y][day][used] + 1;				
 				}
 
 			} 
@@ -119,14 +123,20 @@ int BFS(){
 			else if(B[nx][ny] == 0) {
 				
 				// 이전에 다녀간 적이 없다면 다녀감
-				if(dist[nx][ny][used] == 0){
-					que.ps(Node(nx, ny, used));
-					dist[nx][ny][used] = dist[x][y][used] + 1;	
+				if(dist[nx][ny][!day][used] == 0){
+					que.ps(Node(nx, ny, !day, used));
+					dist[nx][ny][!day][used] = dist[x][y][day][used] + 1;	
 				} 
 				
 			} 
 			
 		}
+		
+		// 같은 위치에서 하루 기다림 -> 이전에 다녀간 적이 없다면 다녀감
+		if(dist[x][y][!day][used] == 0){
+			que.ps(Node(x, y, !day, used));
+			dist[x][y][!day][used] = dist[x][y][day][used] + 1;	
+		} 
 		
 	}
 	
@@ -134,28 +144,28 @@ int BFS(){
 	
 }
 
-
 int main(int argc, char** argv) {
 	
-	// freopen("input.txt", "rt", stdin);
+	freopen("input.txt", "rt", stdin);
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
 	ci2(N, M); ci(K);
 	cig(99999);
-	
+		
+	// 시작점과 도착점이 같으면 1을 출력하고 끝 
 	if(N == M && N == 1){
 		cos(1);
 		return 0;
 	}
 	
+	// 입력받음 
 	rep(i, 1, N + 1){
 		string row;
 		gtl(row);
 		rep(j, 1, M + 1){
 			B[i][j] = row[j - 1] - '0';
 		}
-		
 	}
 	
 	cos(BFS());
